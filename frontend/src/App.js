5758284d744970b3c0481f10a94bba94e87a1244
Anas Yaseen <Anasyaseen1@outlook.com>
2021-04-09 11:24:52 +0300

    Started Building the frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,23 @@
+import Header from "./components/Header";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 const App = () => {
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Switch>
+            <Route exact path="/" component={HomeScreen} />
+            <Route path="/product/:id" component={ProductScreen} />
+          </Switch>
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
 };
 
